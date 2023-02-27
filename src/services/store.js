f83a92fe350a1d8fc2ee186e52e38646272d1e80
oz <ozgrgn@gmail.com>
2023-02-27 12:22:40 +0300

    user login

--- src/services/store.js
+++ src/services/store.js
@@ -1,13 +1,13 @@
 import { writable } from "svelte/store";
 
 
-// const _user = localStorage.getItem("user") && localStorage.getItem("user") != "null"
-//   ? JSON.parse(localStorage.getItem("user"))
-//   : null
-// export const user = writable(_user);
-// user.subscribe((value) => {
-//   localStorage.setItem("user", JSON.stringify(value));
-// });
+const _user = localStorage.getItem("user") && localStorage.getItem("user") != "null"
+  ? JSON.parse(localStorage.getItem("user"))
+  : null
+export const user = writable(_user);
+user.subscribe((value) => {
+  localStorage.setItem("user", JSON.stringify(value));
+});
 
 const _admin = localStorage.getItem("admin") && localStorage.getItem("admin") != "null"
   ? JSON.parse(localStorage.getItem("admin"))
